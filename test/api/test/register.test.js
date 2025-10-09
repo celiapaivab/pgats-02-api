a795f42455556cbe9137bd482ef4d60667e162b8
Flavio <conectef@gmail.com>
2025-10-09 14:47:31 -0300

    adicionei tres testes ao register

--- test/api/test/register.test.js
+++ test/api/test/register.test.js
@@ -29,6 +29,44 @@ describe('Registro', () => {
                 .set('Content-Type', 'application/json')
                 .send(login)
 
+            expect(resposta.status).to.equal(400);
+        })
+
+        it('Validate error without filling in password and username', async () => {
+            const postUser = {
+                "username": "",
+                "password": ""
+            };
+            const resposta = await request("http://localhost:3000")
+                .post('/users/register')
+                .set('Content-Type', 'application/json')
+                .send(postUser)
+
+            expect(resposta.status).to.equal(400);
+        })
+
+        it('Validate error only by filling in username', async () => {
+            const postUser = {
+                "username": "maca",
+                "password": ""
+            };
+            const resposta = await request("http://localhost:3000")
+                .post('/users/register')
+                .set('Content-Type', 'application/json')
+                .send(postUser)
+
+            expect(resposta.status).to.equal(400);
+        })
+
+        it('Validate error only by filling in password', async () => {
+            const postUser = {
+                "username": "",
+                "password": "maca"
+            };
+            const resposta = await request("http://localhost:3000")
+                .post('/users/register')
+                .set('Content-Type', 'application/json')
+                .send(postUser)
 
             expect(resposta.status).to.equal(400);
         })
